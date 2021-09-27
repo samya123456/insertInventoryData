e5102286eb77fff86d3bd5e478a7bd6f33a1c4f2
Samya Nandy <samya.nandy@ibm.com>
2021-09-27 20:58:50 +0530

    db scripts

---
+++ src/main/resources/DB_SCRIPTS/Create_db_table.sql
@@ -0,0 +1,12 @@
+create database App;  
+use App;
+
+CREATE TABLE t_product (
+    product_id integer primary key,
+    product_code varchar(255),
+    product_name varchar(255),
+	product_width integer,
+	product_desc varchar(255),
+	product_company_id integer
+	
+);
