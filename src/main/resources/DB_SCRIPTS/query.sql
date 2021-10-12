e3e625cd16299fe6868754c867fbc2362692dd10
Samya Nandy <samya.nandy@ibm.com>
2021-10-12 19:20:51 +0530

    added

--- src/main/resources/DB_SCRIPTS/query.sql
+++ src/main/resources/DB_SCRIPTS/query.sql
@@ -7,4 +7,14 @@ select prod.product_id,prod.product_code,prod.product_name,comp.company_id
  and comp.company_id = prod_comp.company_id 
  and prod.product_id = ?;
  
-select t1.product_company_branch_id ,t1.branch_product_company_quantity from t_product_company_branch t1 
+select t1.product_company_branch_id ,t1.branch_product_company_quantity from t_product_company_branch t1 ;
+
+
+
+select prod.product_id,prod.product_code,prod.product_name,comp.company_id 
+ ,comp.company_code,comp.company_name,prod_comp.product_company_quantity ,prod_comp.product_purchase_price,prod_comp.product_purchase_currency,
+ prod_comp.product_sale_min_price,prod_comp.product_sale_max_price,prod_comp.product_sale_currency
+ from t_product prod,t_company comp,t_product_company prod_comp 
+ where prod.product_id = prod_comp.product_id 
+ and comp.company_id = prod_comp.company_id 
+  and prod.product_code = ?
